363b7192ef1ffc455c19aa5f13ec4cbba0900c9f
Or <81361496+or2407@users.noreply.github.com>
2022-06-19 17:39:14 +0300

    added option for list of init

--- utils.py
+++ utils.py
@@ -1,74 +1,33 @@
-import os
-from pickle import FALSE
-from collections import defaultdict
-import subprocess
+from networkx import DiGraph
+import utils
+#given a graph, we can ask about it questions
+#enter graph through txt file
+graph = DiGraph()
+f = open("graph.txt", "r")
 
-edgeDict = defaultdict(list)
+flag_edge = 0
+flag_node = 0
+#read the graph from txt file and create the graph
+for i in f.readlines():
+    if "edges:" in i:
+        flag_edge = 1
+        flag_node = 0
+        continue
+    if "nodes:" in i:
+        flag_edge = 0
+        flag_node = 1
+        continue
 
-def graphList(graph):
-    for edge in graph.edges():
-        edgeDict[edge[0]].append(edge[1])
+    if flag_edge == 1:
+        str_tmp = i.split(',')
+        graph.add_edge(int(str_tmp[0]),int(str_tmp[1]))
+    
+    if flag_node == 1:
+        str_tmp = i.split(',')
+        for j in str_tmp:
+            graph.add_node(int(j))
 
-def writeStart(filename, graph, init):
-    #if os.path.exists(filename):
-        #os.remove(filename)  #create new file
+init = [1,2]
 
-    #write beginning of smv file
-    with open(filename, 'w') as fw:
-        fw.write("MODULE main\n\nVAR\n	nodes : ")
-        lw = '{'
-        for i in graph.nodes():
-            lw = lw + 'q' + str(i) + ', '
-        lw = lw[:-2]
-        fw.write(lw)
-        fw.write("};\n")
-        fw.write("\n\nASSIGN")
-        fw.write("			\n\n	init(nodes) := q" + str(init) + ";\n")
-
-        fw.write("    next(nodes) := case\n")
-        lw = ""
-        for key in edgeDict.keys():
-            lw = lw + ("        nodes = q"+str(key)+" : {")
-            for node in edgeDict[key]:
-                lw = lw + "q" + str(node) + ","
-            lw = lw[:-1]
-            fw.write(lw)
-            fw.write("};\n")
-            lw = ""
-        fw.write("        TRUE : nodes;\n\n")
-        fw.write("        esac;\n\n")
-        fw.write("LTLSPEC")
-        fw.write("        (G F(nodes = q4))")
-
-
-# main function of writing the smv file
-def writeSmv(graph, init):
-    graphList(graph)
-    filename_main = 'demo.smv'
-    if os.path.exists(filename_main):
-        os.remove(filename_main)
-    with open(filename_main, 'w') as fw:
-        writeStart(filename_main, graph, init)
-        with open(filename_main, 'r') as fr:
-            for line in fr:
-                fw.write(line)
-
-
-# run smv file and check the result
-def runSmv():
-    smv_file = f'demo.smv'
-    output = subprocess.check_output(['nuXmv', smv_file], shell=True).splitlines()
-    ans = False if 'false' in str(output) else True
-    print(ans)
-    moveList=list()
-   
-    if not ans:
-        loop_vecs = str(b''.join(output))
-        chunks = loop_vecs.split(' ')
-        FLAG=False
-        for i in range(len(chunks)) :
-            if chunks[i] == 'Counterexample':
-                FLAG=True
-            if chunks[i]== 'nodes' and FLAG:
-                moveList.append(chunks[i+2])
-        print(moveList)
+utils.writeSmv(graph, init)
+utils.runSmv()
